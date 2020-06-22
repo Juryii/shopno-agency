f056f5b1f40659160d2ed0cc8b52d6595b150c9a
Yurii <polyanskii2118@gmail.com>
2020-06-22 13:50:20 +0600

    delete scss styles

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('.about-us__slider').slick(
+    {
+      dots: true,
+      accessibility: false,
+      // autoplay: true,
+      arrows: false,
+      adaptiveHeight: true,
+      // centerMode: true,
+      // centerPadding: '60px',
+    }
+  );
+});
