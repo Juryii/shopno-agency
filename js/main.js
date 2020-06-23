bd6ee8cb6737bd034412c9cca2afdd4c4fd61133
Yurii <polyanskii2118@gmail.com>
2020-06-23 19:22:20 +0600

    add styles header, our-services, small-team

--- js/main.js
+++ js/main.js
@@ -11,3 +11,13 @@
 //     }
 //   );
 // });
+$(document).ready(function () {
+  $('.our-services__card-btn').click(function(event ) {
+    $(event.target).closest('.our-services__card').addClass('active');
+
+  });
+  $('.close-more-btn').click(function(event ) {
+    $(event.target).closest('.our-services__card').removeClass('active');
+
+  });
+});
